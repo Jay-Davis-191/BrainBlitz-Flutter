b64ff0f8bd6ee8f72dc99f8830ae5233e05d03f9
Jay Davis <jayddavis191@gmail.com>
2023-12-31 12:46:21 +1000

    Add Flashcard pop-up functionality finished
    Add Flashcard pop-up window is finished and successfully adds flashcards and checks if the answer field is empty

--- student_helper_flutter/lib/screens/screen_flashcard.dart
+++ student_helper_flutter/lib/screens/screen_flashcard.dart
@@ -46,6 +46,63 @@ class _FlashcardScreenState extends State<FlashcardScreen> {
     }
   }
 
+  void _showAddFlashcardDialog() {
+    TextEditingController questionController = TextEditingController();
+    TextEditingController answerController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add Reminder'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: questionController,
+                decoration: InputDecoration(labelText: 'Question'),
+              ),
+              SizedBox(height: 8.0),
+              TextFormField(
+                controller: answerController,
+                decoration: InputDecoration(labelText: 'Answer'),
+              ),
+            ],
+          ),
+            actions: [
+              TextButton(
+                child: Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: Text('Add'),
+                onPressed: () {
+                  if (answerController.text.isNotEmpty) {
+                  setState(() {
+                      flashcards.add({
+                        'question': questionController.text,
+                        'answer': answerController.text,
+                      });
+
+                    });
+                    Navigator.of(context).pop();  // Close the dialog
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text('You must provide an answer'),
+                    ));
+                  }
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+  
+
   void _addFlashcard() {
     setState(() {
       flashcards.add({
@@ -73,7 +130,7 @@ class _FlashcardScreenState extends State<FlashcardScreen> {
       answerText: flashcards[currentIndex]['answer']!,
       onNext: _goToNextFlashcard,
       onPrevious: _goToPreviousFlashcard,
-      onAddFlashcards: _addFlashcard,
+      onAddFlashcards: _showAddFlashcardDialog,
       onDeleteFlashcards: _deleteFlashcard,
       )
       ],
