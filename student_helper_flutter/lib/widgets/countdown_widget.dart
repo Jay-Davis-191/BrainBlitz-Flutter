5081ecf599fc769fb8688705dcf840d5b66c4d51
Jay Davis <jayddavis191@gmail.com>
2024-01-01 18:40:17 +1000

    Finished 6 random numbers and required number generation functionality
    Updated code to finish how the 6 random numbers and the required number are generated. Each step of the solution is added to a list to show later in the pop-up.Making the pop-up is next. Finished how the 6 random numers

--- student_helper_flutter/lib/widgets/countdown_widget.dart
+++ student_helper_flutter/lib/widgets/countdown_widget.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -8,18 +9,21 @@ class CountdownWidget extends StatefulWidget {
 
 class _CountdownWidgetState extends State<CountdownWidget> {
   int requiredNumber = 0; // Example required number
+  List<int> presetNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100]; // Example random numbers
   List<int> setNumbers = []; 
-  String solutionWorking = ''; 
-
+  int currentTotal = 0; 
+  int prevTotal = 0; 
+  List<String> calculatons = [];  // List that contains the entire list of calculations made. Used in the Solution pop-up window
 
   @override
   void initState() {
     super.initState(); 
     getRandomNumbers(); 
+    calculateRequiredNumber(); 
   }
 
+
   void getRandomNumbers() {
-    List<int> presetNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100]; // Example random numbers
     List<int> randomNumbers = []; // Example random numbers
     Random random = Random(); 
 
@@ -30,11 +34,98 @@ class _CountdownWidgetState extends State<CountdownWidget> {
     setNumbers = randomNumbers;  
   }
 
-  void _restartNumbers() {
-    // Logic to restart the numbers
+
+  void calculateRequiredNumber() {
+    bool status; 
+    currentTotal = 0; 
+    prevTotal = 0; 
+
+    List<int> setNumbersCopy = List.from(setNumbers); 
+    do {
+      Random random = Random();
+      int index = random.nextInt(setNumbersCopy.length); 
+      int firstNum = setNumbersCopy.elementAt(index);
+
+      setNumbersCopy.removeAt(index); 
+
+      if (setNumbersCopy.length < 5) {
+        do {
+          status = updateRequiredNumber(firstNum);
+        } while (!status);
+        prevTotal = currentTotal; 
+      }
+      else { 
+        prevTotal = firstNum; 
+      }
+    } while(setNumbersCopy.isNotEmpty); 
+    requiredNumber = currentTotal;
   }
 
+
+  bool updateRequiredNumber(int num1) {
+    Random random = Random(); 
+    List<String> operatorsList = ['+', '-', '*', '/'];
+    String operator = operatorsList.elementAt(random.nextInt(operatorsList.length));
+
+    switch (operator) {
+      case '+':
+        currentTotal = prevTotal + num1; 
+        calculatons.add("$prevTotal + $num1 = $currentTotal"); 
+        return true; 
+
+      case '-':
+        if (prevTotal > num1) {
+          currentTotal = prevTotal - num1; 
+          calculatons.add("$prevTotal - $num1 = $currentTotal"); 
+          return true; 
+        }
+        else if (prevTotal < num1) {
+          currentTotal = num1 - prevTotal; 
+          calculatons.add("$num1 - $prevTotal = $currentTotal"); 
+          return true; 
+        }
+        else {
+          return false; 
+        }
+        
+      case '*':
+        currentTotal = prevTotal * num1; 
+        calculatons.add("$prevTotal * $num1 = $currentTotal"); 
+        return true; 
+
+      case '/':
+        if (prevTotal > num1 && prevTotal % num1 == 0) {
+          currentTotal = prevTotal / num1 as int; 
+          calculatons.add("$prevTotal + $num1 = $currentTotal"); 
+          return true; 
+        }
+        else if (prevTotal < num1 && num1 % prevTotal == 0) {
+          currentTotal = num1 / prevTotal as int; 
+          calculatons.add("$prevTotal + $num1 = $currentTotal"); 
+          return true; 
+        }
+        else {
+          return false; 
+        }
+
+      default: 
+        return false; 
+    }
+  }
+
+
+  void _restartNumbers() {
+    setState(() {
+      getRandomNumbers();
+    });
+  }
+
+
   void _skipToNextSet() {
+    setState(() {
+      getRandomNumbers();
+      calculateRequiredNumber(); 
+    });
     // Logic to skip to the next set of numbers
   }
 
