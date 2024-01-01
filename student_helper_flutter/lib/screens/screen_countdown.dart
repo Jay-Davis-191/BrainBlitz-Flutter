cff28ee3d705395c8943146a85666c2e4a37a4b7
Jay Davis <jayddavis191@gmail.com>
2024-01-01 19:20:59 +1000

    Solution pop-up window finished
    Pop-up window on the Countdown page that shows the solution is finished. Fixed little errors after the pop-up implementation.

--- student_helper_flutter/lib/screens/screen_countdown.dart
+++ student_helper_flutter/lib/screens/screen_countdown.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:student_helper_flutter/widgets/countdown_widget.dart';
 
@@ -7,6 +9,163 @@ class CountdownScreen extends StatefulWidget {
 }
 
 class _CountdownScreenState extends State<CountdownScreen> {
+
+int requiredNumber = 0; // Example required number
+  List<int> presetNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100]; // Example random numbers
+  List<int> setNumbers = []; 
+  int currentTotal = 0; 
+  int prevTotal = 0; 
+  List<String> calculations = [];  // List that contains the entire list of calculations made. Used in the Solution pop-up window
+
+  @override
+  void initState() {
+    super.initState(); 
+    getRandomNumbers(); 
+    calculateRequiredNumber(); 
+  }
+  
+  
+  void getRandomNumbers() {
+    setState(() {
+      List<int> randomNumbers = []; // Example random numbers
+      Random random = Random(); 
+
+      for (int i = 0; i < 6; i++) {
+        int randomNumber = presetNumbers[random.nextInt(presetNumbers.length)]; 
+        randomNumbers.add(randomNumber);
+      }
+      setNumbers = randomNumbers;    
+    });
+    
+  }
+
+
+  void calculateRequiredNumber() {
+    setState(() {
+      bool status; 
+      currentTotal = 0; 
+      prevTotal = 0; 
+      calculations = []; 
+  
+      List<int> setNumbersCopy = List.from(setNumbers); 
+      do {
+        Random random = Random();
+        int index = random.nextInt(setNumbersCopy.length); 
+        int firstNum = setNumbersCopy.elementAt(index);
+  
+        setNumbersCopy.removeAt(index); 
+  
+        if (setNumbersCopy.length < 5) {
+          do {
+            status = updateRequiredNumber(firstNum);
+          } while (!status);
+          prevTotal = currentTotal; 
+        }
+        else { 
+          prevTotal = firstNum; 
+        }
+      } while(setNumbersCopy.isNotEmpty); 
+      requiredNumber = currentTotal;
+      });
+
+  }
+
+
+  bool updateRequiredNumber(int num1) {
+    Random random = Random(); 
+    List<String> operatorsList = ['+', '-', '*', '/'];
+    String operator = operatorsList.elementAt(random.nextInt(operatorsList.length));
+
+    switch (operator) {
+      case '+':
+        currentTotal = prevTotal + num1; 
+        calculations.add("$prevTotal + $num1 = $currentTotal"); 
+        return true; 
+
+      case '-':
+        if (prevTotal > num1) {
+          currentTotal = prevTotal - num1; 
+          calculations.add("$prevTotal - $num1 = $currentTotal"); 
+          return true; 
+        }
+        else if (prevTotal < num1) {
+          currentTotal = num1 - prevTotal; 
+          calculations.add("$num1 - $prevTotal = $currentTotal"); 
+          return true; 
+        }
+        else {
+          return false; 
+        }
+        
+      case '*':
+        currentTotal = prevTotal * num1; 
+        calculations.add("$prevTotal * $num1 = $currentTotal"); 
+        return true; 
+
+      case '/':
+        if (prevTotal > num1 && prevTotal % num1 == 0) {
+          currentTotal = prevTotal / num1 as int; 
+          calculations.add("$prevTotal + $num1 = $currentTotal"); 
+          return true; 
+        }
+        else if (prevTotal < num1 && num1 % prevTotal == 0) {
+          currentTotal = num1 / prevTotal as int; 
+          calculations.add("$prevTotal + $num1 = $currentTotal"); 
+          return true; 
+        }
+        else {
+          return false; 
+        }
+
+      default: 
+        return false; 
+    }
+  }
+
+
+  void _restartNumbers() {
+      getRandomNumbers();
+  }
+
+
+  void _skipToNextSet() {
+      getRandomNumbers();
+      calculateRequiredNumber(); 
+    // Logic to skip to the next set of numbers
+  }
+
+
+  void example() {
+
+  }
+
+  void _showSolution() {
+      showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Solution'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: calculations.map((item) {
+                return Text(item); // Each item on a new line
+              }).toList(),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,8 +173,16 @@ class _CountdownScreenState extends State<CountdownScreen> {
         title: Text('Countdown Math Game'),
       ),
       body: Center(
-        child: CountdownWidget(),
+        child: CountdownWidget(
+          requiredNumber: requiredNumber,
+          setNumbers: setNumbers, 
+          onShowSolution: _showSolution,
+          onRestartNumbers: _restartNumbers, 
+          onSkip: _skipToNextSet, 
+        )
+        
       ),
     );
   }
 }
+
--- student_helper_flutter/lib/widgets/countdown_widget.dart
+++ student_helper_flutter/lib/widgets/countdown_widget.dart
@@ -3,136 +3,26 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 class CountdownWidget extends StatefulWidget {
+  final int requiredNumber; 
+  final List<int> setNumbers; 
+  final VoidCallback onShowSolution; 
+  final VoidCallback onRestartNumbers; 
+  final VoidCallback onSkip; 
+
+  CountdownWidget ({
+    required this.requiredNumber, 
+    required this.setNumbers, 
+    required this.onShowSolution, 
+    required this.onRestartNumbers, 
+    required this.onSkip,
+  });
+
   @override
   _CountdownWidgetState createState() => _CountdownWidgetState();
 }
 
 class _CountdownWidgetState extends State<CountdownWidget> {
-  int requiredNumber = 0; // Example required number
-  List<int> presetNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100]; // Example random numbers
-  List<int> setNumbers = []; 
-  int currentTotal = 0; 
-  int prevTotal = 0; 
-  List<String> calculatons = [];  // List that contains the entire list of calculations made. Used in the Solution pop-up window
-
-  @override
-  void initState() {
-    super.initState(); 
-    getRandomNumbers(); 
-    calculateRequiredNumber(); 
-  }
-
-
-  void getRandomNumbers() {
-    List<int> randomNumbers = []; // Example random numbers
-    Random random = Random(); 
-
-    for (int i = 0; i < 6; i++) {
-      int randomNumber = presetNumbers[random.nextInt(presetNumbers.length)]; 
-      randomNumbers.add(randomNumber);
-    }
-    setNumbers = randomNumbers;  
-  }
-
-
-  void calculateRequiredNumber() {
-    bool status; 
-    currentTotal = 0; 
-    prevTotal = 0; 
-
-    List<int> setNumbersCopy = List.from(setNumbers); 
-    do {
-      Random random = Random();
-      int index = random.nextInt(setNumbersCopy.length); 
-      int firstNum = setNumbersCopy.elementAt(index);
-
-      setNumbersCopy.removeAt(index); 
-
-      if (setNumbersCopy.length < 5) {
-        do {
-          status = updateRequiredNumber(firstNum);
-        } while (!status);
-        prevTotal = currentTotal; 
-      }
-      else { 
-        prevTotal = firstNum; 
-      }
-    } while(setNumbersCopy.isNotEmpty); 
-    requiredNumber = currentTotal;
-  }
-
-
-  bool updateRequiredNumber(int num1) {
-    Random random = Random(); 
-    List<String> operatorsList = ['+', '-', '*', '/'];
-    String operator = operatorsList.elementAt(random.nextInt(operatorsList.length));
-
-    switch (operator) {
-      case '+':
-        currentTotal = prevTotal + num1; 
-        calculatons.add("$prevTotal + $num1 = $currentTotal"); 
-        return true; 
-
-      case '-':
-        if (prevTotal > num1) {
-          currentTotal = prevTotal - num1; 
-          calculatons.add("$prevTotal - $num1 = $currentTotal"); 
-          return true; 
-        }
-        else if (prevTotal < num1) {
-          currentTotal = num1 - prevTotal; 
-          calculatons.add("$num1 - $prevTotal = $currentTotal"); 
-          return true; 
-        }
-        else {
-          return false; 
-        }
-        
-      case '*':
-        currentTotal = prevTotal * num1; 
-        calculatons.add("$prevTotal * $num1 = $currentTotal"); 
-        return true; 
-
-      case '/':
-        if (prevTotal > num1 && prevTotal % num1 == 0) {
-          currentTotal = prevTotal / num1 as int; 
-          calculatons.add("$prevTotal + $num1 = $currentTotal"); 
-          return true; 
-        }
-        else if (prevTotal < num1 && num1 % prevTotal == 0) {
-          currentTotal = num1 / prevTotal as int; 
-          calculatons.add("$prevTotal + $num1 = $currentTotal"); 
-          return true; 
-        }
-        else {
-          return false; 
-        }
-
-      default: 
-        return false; 
-    }
-  }
-
-
-  void _restartNumbers() {
-    setState(() {
-      getRandomNumbers();
-    });
-  }
-
-
-  void _skipToNextSet() {
-    setState(() {
-      getRandomNumbers();
-      calculateRequiredNumber(); 
-    });
-    // Logic to skip to the next set of numbers
-  }
-
-  void _showSolution() {
-    // Logic to display the solution in a popup
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -142,18 +32,18 @@ class _CountdownWidgetState extends State<CountdownWidget> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             ElevatedButton(
-              onPressed: _restartNumbers,
+              onPressed: widget.onRestartNumbers,
               child: Text('Restart'),
             ),
             ElevatedButton(
-              onPressed: _skipToNextSet,
+              onPressed: widget.onSkip,
               child: Text('Pass'),
             ),
           ],
         ),
         SizedBox(height: 20),
         Text(
-          'Required Number: $requiredNumber',
+          'Required Number:${widget.requiredNumber}',
           style: TextStyle(fontSize: 24),
         ),
         SizedBox(height: 20),
@@ -161,56 +51,56 @@ class _CountdownWidgetState extends State<CountdownWidget> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
           ElevatedButton(
-            onPressed: _skipToNextSet,
-            child: Text(setNumbers.elementAt(0).toString()),
+            onPressed: widget.onSkip,
+            child: Text(widget.setNumbers.elementAt(0).toString()),
             ),
           ElevatedButton(
-            onPressed: _skipToNextSet,
-            child: Text(setNumbers.elementAt(1).toString()),
+            onPressed: widget.onSkip,
+            child: Text(widget.setNumbers.elementAt(1).toString()),
             ),
           ElevatedButton(
-            onPressed: _skipToNextSet,
-            child: Text(setNumbers.elementAt(2).toString()),
+            onPressed: widget.onSkip,
+            child: Text(widget.setNumbers.elementAt(2).toString()),
             ),
           ElevatedButton(
-            onPressed: _skipToNextSet,
-            child: Text(setNumbers.elementAt(3).toString()),
+            onPressed: widget.onSkip,
+            child: Text(widget.setNumbers.elementAt(3).toString()),
             ),
           ElevatedButton(
-            onPressed: _skipToNextSet,
-            child: Text(setNumbers.elementAt(4).toString()),
+            onPressed: widget.onSkip,
+            child: Text(widget.setNumbers.elementAt(4).toString()),
             ),
           ElevatedButton(
-            onPressed: _skipToNextSet,
-            child: Text(setNumbers.elementAt(5).toString()),
+            onPressed: widget.onSkip,
+            child: Text(widget.setNumbers.elementAt(5).toString()),
             ),  
           ]
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             ElevatedButton(
-              onPressed: _showSolution,
-              child: Text('+'),
+              onPressed: widget.onSkip, // This will be changed later to include the operator once selected
+              child: const Text('+'),
             ),
             ElevatedButton(
-              onPressed: _showSolution,
-              child: Text('-'),
+              onPressed: widget.onSkip, // This will be changed later to include the operator once selected
+              child: const Text('-'),
             ),
             ElevatedButton(
-              onPressed: _showSolution,
-              child: Text('x'),
+              onPressed: widget.onSkip, // This will be changed later to include the operator once selected
+              child: const Text('x'),
             ),
             ElevatedButton(
-              onPressed: _showSolution,
-              child: Text('/'),
+              onPressed: widget.onSkip, // This will be changed later to include the operator once selected
+              child: const Text('/'),
             ),
           ],
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         ElevatedButton(
-          onPressed: _showSolution,
+          onPressed: widget.onShowSolution,
           child: Text('Solution'),
         ),
       ],
